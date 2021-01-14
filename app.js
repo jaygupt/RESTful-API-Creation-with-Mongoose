6676749feff7c6111192f589febb61df88ea34c3
Jay Gupta <jaygupt1973@gmail.com>
2021-01-14 01:24:18 -0800

    Implement app.route() for chaining

--- app.js
+++ app.js
@@ -33,46 +33,46 @@ const articleSchema = {
 // create an article model
 const Article = mongoose.model("Article", articleSchema); 
 
-// READ all articles
-app.get("/articles", function(req, res){
-  Article.find({}, function(err, foundArticles){
-    if (!err) {
-      res.send(foundArticles); 
-    } else {
-      res.send(err);
-    }
-  });
-});
+app.route("/articles")
+  .get(function(req, res){
+    // READ all articles
+    Article.find({}, function(err, foundArticles){
+      if (!err) {
+        res.send(foundArticles); 
+      } else {
+        res.send(err);
+      }
+    });
+  })
+   
+  .post(function(req, res){
+    // CREATE an article
+    const newTitle = req.body.title; 
+    const newContent = req.body.content;
+    
+    const newArticle = new Article({
+      title: newTitle,
+      content: newContent
+    });
+    newArticle.save(function(err){
+      if (!err) {
+        res.send("Successfully added a new article."); 
+      } else {
+        res.send(err); 
+      }
+    }); 
+  })
 
-// CREATE an article
-app.post("/articles", function(req, res){
-  const newTitle = req.body.title; 
-  const newContent = req.body.content;
-  
-  const newArticle = new Article({
-    title: newTitle,
-    content: newContent
-  }); 
-
-  newArticle.save(function(err){
-    if (!err) {
-      res.send("Successfully added a new article."); 
-    } else {
-      res.send(err); 
-    }
-  }); 
-});
-
-// DELETE all articles
-app.delete("/articles", function(req, res){
-  Article.deleteMany({}, function(err){
-    if (!err) {
-      res.send("Deleted all articles.");
-    } else {
-      res.send(err); 
-    }
-  });
-});
+  .delete(function(req, res){
+    // DELETE all articles
+    Article.deleteMany({}, function(err){
+      if (!err) {
+        res.send("Deleted all articles.");
+      } else {
+        res.send(err); 
+      }
+    });
+  });  
 
 app.listen(3000, function(){
   console.log("Server started on port 3000."); 
